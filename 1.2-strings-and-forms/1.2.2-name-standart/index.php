fbd784181443e27828420d57875c417a95f16c26
Dmitriy Ribalko <ribalko@yandex.ru>
2019-06-27 22:26:09 +0300

    task 1.2.2 corrected

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -1,12 +1,17 @@
 <?php
 
+    mb_internal_encoding("UTF-8");
+    function mb_ucfirst($text) {
+        return mb_strtoupper(mb_substr($text, 0, 1)) . mb_substr($text, 1);
+    }
+        
     $firstName = $_POST['firstName']; 
     $lastName = $_POST['lastName']; 
     $middleName = $_POST['middleName']; 
 
-    $fullName =  ucfirst($firstName) . ' ' . ucfirst($lastName) . ' ' . ucfirst($middleName);
+    $fullName =  mb_ucfirst($firstName) . ' ' . mb_ucfirst($lastName) . ' ' . mb_ucfirst($middleName);
     $fio = strtoupper(mb_substr($firstName,0,1) . mb_substr($lastName,0,1) . mb_substr($middleName,0,1));
-    $surnameAndIntials = ucfirst($firstName) . ' ' . strtoupper(mb_substr($lastName,0,1)). '.'  . strtoupper(mb_substr($middleName,0,1)) . '.';
+    $surnameAndIntials = mb_ucfirst($firstName) . ' ' . strtoupper(mb_substr($lastName,0,1)). '.'  . strtoupper(mb_substr($middleName,0,1)) . '.';
 
     echo "Полное имя: " . $fullName . "<br>";
     echo "Фамилия и инициалы: " . $fio . "<br>";
